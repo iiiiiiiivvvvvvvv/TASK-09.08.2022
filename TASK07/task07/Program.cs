6dec7342845a29c1b73e9784686d6583025ab2c0
Igor <109410390+iiiiiiiivvvvvvvv@users.noreply.github.com>
2022-08-09 15:27:19 +0500

    Add files via upload

---
+++ TASK07/task07/Program.cs
@@ -0,0 +1,8 @@
+﻿//Третья цифра в 3х значном числе
+Console.Clear();
+System.Console.WriteLine();
+
+int number = new Random().Next(100, 1000);
+
+System.Console.WriteLine($"Последняя цифра {number}  цифра {number%10}");
+System.Console.WriteLine();
