99b0af41103a4779c8b5fa9abf98982c5cbdfe7b
Igor <109410390+iiiiiiiivvvvvvvv@users.noreply.github.com>
2022-08-09 15:29:16 +0500

    Add files via upload

---
+++ TASK09/task09/Program.cs
@@ -0,0 +1,7 @@
+﻿// Удалить вторую цифру трёхзначного числа
+Console.WriteLine("Ввести трехзначное число:");
+int n = int.Parse(Console.ReadLine());
+int number1 = n % 10;
+int number3 = n / 100;
+int result = number3 * 10 + number1;
+Console.WriteLine("Полученный результат " + result);
