7b105b4f68e90a18bd55fc4401c344d985143e17
Igor <109410390+iiiiiiiivvvvvvvv@users.noreply.github.com>
2022-08-18 12:27:28 +0500

    Add files via upload

---
+++ TASK13/task13/Program.cs
@@ -0,0 +1,15 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+Console.WriteLine("Введите число a");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число b");
+int b = int.Parse(Console.ReadLine());
+if(a == b * b)
+{
+    Console.WriteLine("Является квадратом");
+}
+else
+{
+   Console.WriteLine("Не является квадратом");  
+}
