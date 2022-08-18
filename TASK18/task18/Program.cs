527e2dd293a84b9442bc09c31753b08448fc0e1b
Igor <109410390+iiiiiiiivvvvvvvv@users.noreply.github.com>
2022-08-18 12:30:46 +0500

    Add files via upload

---
+++ TASK18/task18/Program.cs
@@ -0,0 +1,10 @@
+﻿// Определить количество цифр в числе
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int i = 0;
+for(i = 0; n >= 1; i++)
+{
+    n = n / 10;
+}
+Console.WriteLine(i);
