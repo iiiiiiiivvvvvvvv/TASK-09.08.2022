469f93df9d849611c04a0f79a9dc9daf913056a9
Igor <109410390+iiiiiiiivvvvvvvv@users.noreply.github.com>
2022-08-18 12:28:09 +0500

    Add files via upload

---
+++ TASK14/task14/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+int quarterNum = new Random().Next(1, 5);
+string range = "";
+string num = "";
+if (quarterNum == 1) range = "x > 0, y > 0";
+else if (quarterNum == 2) range = "x > 0, y < 0";
+else if (quarterNum == 3) range = "x < 0, y < 0";
+else if (quarterNum == 4) range = "x < 0, y > 0";
+Console.WriteLine($"Для {quarterNum} -й четверти задать точки ({range})");
+Console.WriteLine();
