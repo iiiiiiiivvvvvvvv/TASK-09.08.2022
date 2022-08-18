6f726522ad6c74f6df06f7a057407280819b9408
Igor <109410390+iiiiiiiivvvvvvvv@users.noreply.github.com>
2022-08-18 12:29:29 +0500

    Add files via upload

---
+++ TASK16/task16/Program.cs
@@ -0,0 +1,10 @@
+﻿//Найти кубы чисел от 1 до N
+
+Console.WriteLine("Введите число N");
+int i = 1;
+int N = int.Parse(Console.ReadLine());
+ Console.WriteLine("Число" + "\t" + "Куб");
+ for(i = 1; i<= N; i++)
+ {
+    Console.WriteLine(i + "\t" + i * i * i);
+ }
