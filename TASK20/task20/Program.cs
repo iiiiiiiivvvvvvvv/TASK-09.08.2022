eadc1a1243cbf3003c13503e55c006a2f915e94e
Igor <109410390+iiiiiiiivvvvvvvv@users.noreply.github.com>
2022-08-18 12:32:01 +0500

    Add files via upload

---
+++ TASK20/task20/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Введите число N: ");
+int number = int.Parse(Console.ReadLine());
+
+int factorial(int num)
+{
+    int result = 1;
+    for (int i = 1; i <= num; i++)
+    {
+    result=result*i;
+    }
+    return result;
+}
+Console.WriteLine(factorial(number));
