b5531105977c5b4aea0dc8df76e5baf8c4ef2703
Igor <109410390+iiiiiiiivvvvvvvv@users.noreply.github.com>
2022-08-18 12:26:46 +0500

    Add files via upload

---
+++ TASK12/task12/Program.cs
@@ -0,0 +1,9 @@
+﻿//Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.WriteLine();
+int numberDay = new Random(). Next(1, 8);
+string[] day = { " Понедельник ", " Вторник ", " Среда ", " Четверг ", " Пятница ", " Суббота ", " Воскрксенье " };
+string result = "";
+if (numberDay > 5) result = "Выходной день";
+else result = "Рабочий день";
+Console.WriteLine();
+Console.WriteLine ($"{numberDay}-й день недели - это {day [numberDay-1]} {result}");
