e5bcd6a5d3cd067d1e6cebaa657da8ba1625b3da
Igor <109410390+iiiiiiiivvvvvvvv@users.noreply.github.com>
2022-08-18 12:31:24 +0500

    Add files via upload

---
+++ TASK19/task19/Program.cs
@@ -0,0 +1,12 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int i = 0;
+int sum = 0;
+for(i = 0; n>=1; i++ )
+{
+    sum += n % 10;
+    n = n / 10;
+}
+Console.WriteLine("Сумма цифр равна " + sum);
