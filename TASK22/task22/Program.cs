9b1abcb9780c17b0f2290ba301a5a9281e2202a9
Igor <109410390+iiiiiiiivvvvvvvv@users.noreply.github.com>
2022-08-22 09:20:07 +0500

    Add files via upload

---
+++ TASK22/task22/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+
+int[] array = new int[8];
+string F(int[] array)
+{
+    string result = string.Empty;
+    for (int m = 0; m < array.Length; m++)
+    {
+        array[m] = new Random().Next(0, 2);
+    }
+    for (int m = 0; m < array.Length; m++)
+    {
+        result += $"{array[m]}  ";
+    }
+    return result;
+}
+Console.WriteLine(F(array));
