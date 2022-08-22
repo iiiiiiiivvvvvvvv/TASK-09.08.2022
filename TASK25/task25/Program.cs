e124fe6815a25778ac787d7367b9c04a01fbb45d
Igor <109410390+iiiiiiiivvvvvvvv@users.noreply.github.com>
2022-08-22 09:22:23 +0500

    Add files via upload

---
+++ TASK25/task25/Program.cs
@@ -0,0 +1,20 @@
+﻿// Определить, присутствует ли в заданном массиве, некоторое число
+
+int[] array = new int[10];
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 20);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+Console.WriteLine("Введите искомое число: ");
+int number = int.Parse(Console.ReadLine());
+
+for (int j = 0; j < array.Length; j++)
+{
+    if (array[j] == number) count = 1;
+}
+if (count > 0) Console.WriteLine("Это число присутствует в массиве");
+else Console.WriteLine("Искомое число в массиве отсутствует");
