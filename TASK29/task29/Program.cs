6349b622d66132c4a8abac87ad95d47591705ee3
Igor <109410390+iiiiiiiivvvvvvvv@users.noreply.github.com>
2022-08-22 09:25:33 +0500

    Add files via upload

---
+++ TASK29/task29/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+int N = 10;
+int[] array = new int[N];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 10);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+int j = array.Length - 1;
+for (int i = 0; i < (array.Length + 1) / 2; i++)
+{
+    Console.Write(array[i] * array[j] + " ");
+    j--;
+}
---
+++ TASK30/task30/Program.cs
@@ -0,0 +1,14 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+
+int[] array = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 20 };
+int max = array[0];
+int min = array[0];
+
+for (int j = 0; j < array.Length; j++)
+{
+    if (array[j] > max) max = array[j];
+    if (array[j] < min) min = array[j];
+}
+Console.WriteLine();
+Console.Write("Разница между максимальным и минимальным элементом равна - ");
+Console.Write(max - min);
