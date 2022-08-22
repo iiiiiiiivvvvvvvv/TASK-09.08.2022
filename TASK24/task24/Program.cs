5893ae171ce7a055edd4a2cc2f254fd3f4b8957b
Igor <109410390+iiiiiiiivvvvvvvv@users.noreply.github.com>
2022-08-22 09:21:39 +0500

    Add files via upload

---
+++ TASK24/task24/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу замену элементов массива на противоположные
+
+int[] array = new int[12];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 10);
+    Console.Write(array[i] + " "); 
+}
+Console.WriteLine();
+for (int j = 0; j < array.Length; j++)
+{
+    array[j] = array[j] * -1;
+    Console.Write(array[j]+ " ");
+}
