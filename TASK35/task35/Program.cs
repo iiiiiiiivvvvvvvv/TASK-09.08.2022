9ca383dae0e62e6fedb6f6710caf8b74f4964113
Igor <109410390+iiiiiiiivvvvvvvv@users.noreply.github.com>
2022-08-25 10:05:06 +0500

    Add files via upload

---
+++ TASK35/task35/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+void FillArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            array[m, n] = m + n;
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            Console.Write($"{array[m, n]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int m = 5;
+int n = 5;
+int[,] array = new int[m, n];
+
+FillArray(array);
+PrintArray(array);
