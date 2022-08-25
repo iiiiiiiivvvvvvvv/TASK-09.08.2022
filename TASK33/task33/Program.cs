699a6d6d978228258846123dab4991be92991281
Igor <109410390+iiiiiiiivvvvvvvv@users.noreply.github.com>
2022-08-25 08:55:58 +0500

    Add files via upload

---
+++ TASK33/task33/Program.cs
@@ -0,0 +1,18 @@
+﻿//Написать программу копирования массива
+int[] array = new int[10];
+int arrayCopy;
+
+Console.Write("Оригинал ");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 50);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+array[2] = 17; // для проверки
+Console.Write("   Копия ");
+for (int i = 0; i < array.Length; i++)
+{
+    arrayCopy = array[i];
+    Console.Write(arrayCopy + " ");
+}
