7055b511f0d9ba7f2f6ef0c62bf65987e7c201bb
Igor <109410390+iiiiiiiivvvvvvvv@users.noreply.github.com>
2022-09-02 10:24:44 +0500

    Add files via upload

---
+++ TASK46/task46/Program.cs
@@ -0,0 +1,11 @@
+﻿// 67. Показать натуральные числа от N до 1, N задано.
+
+//N = 5. -> "5, 4, 3, 2, 1"
+
+void PrintNumber(int n)
+{
+    if (n < 1) return;
+    Console.Write(n + " ");
+    PrintNumber(n - 1);
+}
+PrintNumber(10);
---
+++ TASK48/task48/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+// M = 1; N = 4 -> 10
+// M = 4; N = 8. -> 30
+
+int Sum(int m, int n)
+{
+    if (n == m) return n;
+    return m + Sum(m + 1, n);
+}
+Console.WriteLine(Sum(1, 10));
